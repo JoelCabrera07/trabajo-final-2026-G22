45d1158cbc8d58309bba65aa60748b4fd3b5b38c
Joel Cabrera <joelcabrera1007@gmail.com>
2026-09-16 01:12:27 -0300

    Sistema de colisiones: interfaz Colisionable, deteccion jugador-plataforma con chequeo de aterrizaje, jugador-enemigo, y plataforma normal

--- trabajo-final-2026-G22/src/Plataforma.java
+++ trabajo-final-2026-G22/src/Plataforma.java
@@ -1,11 +1,12 @@
 import java.awt.Color;
 
-public abstract class Plataforma extends Entidad {
+public abstract class Plataforma extends Entidad implements Colisionable {
     
     public Plataforma(String nombre, double x, double y, int alto, int ancho, Color color,int hp, int ataque) {
         // Llama al constructor de Entidad (HP 1, Ataque 0)
         super(nombre, x, y, alto, ancho, color, hp,ataque); // 1 seria la vida de la plataforma, 0 el ataque porque es inofensivo
     }
     // Obligamos a todas las plataformas hijas a tener este metodo
+    @Override
     public abstract void aplicarEfectoColision(Heroe h);
 }
